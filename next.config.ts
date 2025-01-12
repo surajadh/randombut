8e5ab0cf70c753dd24caf1a938cf8fd5245484f3
Suraj Adhikari <suraj.adhikari@yonomi.co>
2025-01-12 15:46:28 -0600

    Fix for image links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/randombut",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
